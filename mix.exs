3d5f439d3f3c5a80e666c71aa14a3ab06b5f0876
Kevin Etore <kevin.etore@nedap.com>
2017-09-18 20:20:52 +0200

    Add oauth2 dependency

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule FitbitClient.Mixfile do
   def application do
     [
       mod: {FitbitClient, []},
-      extra_applications: [:logger]
+      extra_applications: [:logger, :oauth2]
     ]
   end
 
@@ -40,7 +40,8 @@ defmodule FitbitClient.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:oauth2, "~> 0.9"}
     ]
   end
 
