74211f523f118013a6dfa5d59287a8213f43c3fd
Kevin Etore <kevin.etore@nedap.com>
2017-10-06 13:03:59 +0200

    Fixed Caren Authorization Code Grant flow

--- web/controllers/caren_auth_controller.ex
+++ web/controllers/caren_auth_controller.ex
@@ -7,10 +7,21 @@ defmodule FitbitClient.CarenAuthController do
   end
 
   def callback(conn, %{"code" => code}) do
-    token = Caren.get_token!(code: code)
+    token = Caren.get_token(code: code)
+    user = OAuth2.Client.get!(token, "/api/v1/user").body
     IO.inspect(token)
-    conn
-      |> put_flash(:info, "Succesfully signed in")
-      |> redirect(to: "/")
+    first_name = user["_embedded"]["person"]["first_name"]
+    email = user["_embedded"]["person"]["email"]
+    changeset = User.changeset(%User{},
+      %{
+        name: first_name,
+        email: email,
+        access_token: token.token.access_token,
+        refresh_token: token.token.refresh_token
+      })
+      Repo.insert!(changeset)
+      conn
+        |> put_flash(:info, "Welcome #{first_name}.")
+        |> redirect(to: "/")
   end
 end
--- web/oauth/caren.ex
+++ web/oauth/caren.ex
@@ -2,30 +2,31 @@ defmodule Caren do
   use OAuth2.Strategy
   alias OAuth2.Strategy.AuthCode
 
-  # Public API
-
   def client do
     OAuth2.Client.new([
       strategy: __MODULE__,
       client_id: System.get_env("CAREN_CLIENT_ID"),
       client_secret: System.get_env("CAREN_CLIENT_SECRET"),
       redirect_uri: System.get_env("CAREN_REDIRECT_URI"),
-      site: "https://carenzorgt.nl",
-      authorize_url: "https://carenzorgt.nl/login/oauth/authorize",
-      token_url: "https://carenzorgt.nl/oauth/token"
+      site: "http://localhost:3005",
+      authorize_url: "http://localhost:3005/login/oauth/authorize",
+      token_url: "http://caren.dev:3005/oauth/token"
     ])
+  end
+
+  def redirect_url do
+    OAuth2.Client.authorize_url!(client(), scope: "user.read")
   end
 
   def authorize_url!(params \\ []) do
     OAuth2.Client.authorize_url!(client(), params)
   end
 
-  def get_token!(params \\ [], headers \\ []) do
+  def get_token(params \\ [], headers \\ []) do
     OAuth2.Client.get_token!(client(), params, headers)
   end
 
-  # Strategy Callbacks
-
+  # Strategy Callbacks:
   def authorize_url(client, params) do
     AuthCode.authorize_url(client, params)
   end
@@ -35,4 +36,5 @@ defmodule Caren do
     |> put_param(:client_secret, client().client_secret)
     |> AuthCode.get_token(params, headers)
   end
+
 end
