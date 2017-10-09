ecbcdcc9ed8f73651e2284a22a41dadace685a09
Kevin Etore <kevin.etore@nedap.com>
2017-10-09 22:24:47 +0200

    Changed Caren API call to include Person ID, as well refactored some pattern matches

--- web/controllers/caren_auth_controller.ex
+++ web/controllers/caren_auth_controller.ex
@@ -9,19 +9,18 @@ defmodule FitbitClient.CarenAuthController do
   def callback(conn, %{"code" => code}) do
     token = Caren.get_token(code: code)
     user = OAuth2.Client.get!(token, "/api/v1/user").body
-    IO.inspect(token)
-    first_name = user["_embedded"]["person"]["first_name"]
-    email = user["_embedded"]["person"]["email"]
+    default_user = user["_embedded"]["person"]
     changeset = User.changeset(%User{},
       %{
-        name: first_name,
-        email: email,
+        name: default_user["first_name"],
+        email: default_user["email"],
         access_token: token.token.access_token,
-        refresh_token: token.token.refresh_token
+        refresh_token: token.token.refresh_token,
+        caren_id: default_user["id"]
       })
       Repo.insert!(changeset)
       conn
-        |> put_flash(:info, "Welcome #{first_name}.")
+        |> put_flash(:info, "Welcome #{default_user["first_name"]}.")
         |> redirect(to: "/")
   end
 end
