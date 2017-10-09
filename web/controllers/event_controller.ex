f100b63bd89297594a2cbc89f781f0d530911752
Kevin Etore <kevin.etore@nedap.com>
2017-10-09 15:11:13 +0200

    Created events controller with example iCal data

---
+++ web/controllers/event_controller.ex
@@ -0,0 +1,26 @@
+defmodule FitbitClient.EventController do
+  use FitbitClient.Web, :controller
+
+  plug :action
+
+  def index(conn, _params) do
+    events = [
+    %ICalendar.Event{
+      url: "https://www.floorhelpt.nl/",
+      summary: "Bring some delicious food to Bas Janssen",
+      dtstart: [[2017, 10, 9], [8, 00, 00]],
+      dtend: [[2017, 10, 10], [11, 30, 00]],
+      description: "He has not eaten food in over three days..",
+      location: "Nedap, Groenlo. Parallelweg 2"
+    },
+    %ICalendar.Event{
+      url: "https://www.floorhelpt.nl/",
+      summary: "Bring some disgusting food to Meg",
+      dtstart: [[2017, 10, 9], [13, 00, 00]],
+      dtend: [[2017, 10, 10], [14, 30, 00]],
+      description: "I don't like her, so let's poision her",
+      location: "Leeuwarden, Friesland"
+    }]
+    render conn, events: events
+  end
+end
