276d48ecb0b63ac58af860a93cec0b4194052c3c
Kevin Etore <kevin.etore@nedap.com>
2017-10-09 22:23:37 +0200

    Now rendering ical instead of JSON, should insert this into the database

--- web/controllers/event_controller.ex
+++ web/controllers/event_controller.ex
@@ -3,22 +3,24 @@ defmodule FitbitClient.EventController do
 
   def index(conn, _params) do
     events = [
-    %ICalendar.Event{
-      url: "https://www.floorhelpt.nl/",
-      summary: "Bring some delicious food to Bas Janssen",
-      dtstart: [[2017, 10, 9], [8, 00, 00]],
-      dtend: [[2017, 10, 10], [11, 30, 00]],
-      description: "He has not eaten food in over three days..",
-      location: "Nedap, Groenlo. Parallelweg 2"
-    },
-    %ICalendar.Event{
-      url: "https://www.floorhelpt.nl/",
-      summary: "Bring some disgusting food to Meg",
-      dtstart: [[2017, 10, 9], [13, 00, 00]],
-      dtend: [[2017, 10, 10], [14, 30, 00]],
-      description: "I don't like her, so let's poision her",
-      location: "Leeuwarden, Friesland"
-    }]
-    render conn, events: events
+      %ICalendar.Event{
+        url: "https://www.floorhelpt.nl/",
+        summary: "Bring some delicious food to Bas Janssen",
+        dtstart: {{2017, 10, 09}, {8, 30, 00}},
+        dtend:   {{2017, 10, 09}, {8, 45, 00}},
+        description: "He has not eaten food in over three days..",
+        location: "Nedap, Groenlo. Parallelweg 2"
+      },
+      %ICalendar.Event{
+        url: "https://www.floorhelpt.nl/",
+        summary: "Bring some disgusting food to Meg",
+        dtstart: Timex.now,
+        dtend:   Timex.shift(Timex.now, hours: 3),
+        description: "Not sure why, Meg sounds like a nice name",
+        location: "Leeuwarden, Friesland"
+      },
+    ]
+    ics = %ICalendar{ events: events } |> ICalendar.to_ics
+    render conn, events: ics
   end
 end
