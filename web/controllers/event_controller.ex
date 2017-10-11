23a1327bcc3c9a71b29942174a21a4f98d33b1dc
Kevin Etore <kevin.etore@nedap.com>
2017-10-11 12:04:53 +0200

    Rendering ical with some events, securing this data with JWT will be my next step

--- web/controllers/event_controller.ex
+++ web/controllers/event_controller.ex
@@ -14,11 +14,19 @@ defmodule FitbitClient.EventController do
       %ICalendar.Event{
         url: "https://www.floorhelpt.nl/",
         summary: "Bring some disgusting food to Meg",
-        dtstart: Timex.now,
+        dtstart: {{2017, 10, 11}, {10, 30, 00}},
         dtend:   Timex.shift(Timex.now, hours: 3),
         description: "Not sure why, Meg sounds like a nice name",
         location: "Leeuwarden, Friesland"
       },
+      %ICalendar.Event{
+        url: "https://www.floorhelpt.nl/",
+        summary: "Bring some disgusting food to Steven",
+        dtstart: {{2017, 10, 11}, {8, 30, 00}},
+        dtend:   {{2017, 10, 11}, {10, 30, 00}},
+        description: "Not sure why, Steven sounds like a nice name",
+        location: "Leeuwarden, Friesland"
+      }
     ]
     ics = %ICalendar{ events: events } |> ICalendar.to_ics
     render conn, events: ics
